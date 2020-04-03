638d500da795ee6d20377ce279add965372fc89d
Andrew-Klaas <aklaas2@gmail.com>
2020-04-03 13:28:46 +0000

    update

---
+++ vnet.tf
@@ -0,0 +1,19 @@
+module "network" {
+  source  = "app.terraform.io/aklaas-training/network/azurerm"
+  version = "3.0.1"
+}
+
+
+module "network" {
+  source  = "app.terraform.io/aklaas-training/network/azurerm"
+  resource_group_name = azurerm_resource_group.test.myresourcegroup
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+}
+  
